60d35f600227d9266899a02e2b3fcc7e1d29b47d
Adhara <adharanosalevich@gmail.com>
2023-07-24 07:59:53 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Escribe tu nombre: \t");
+string name = Console.ReadLine();
+Console.WriteLine("Hi, " +  name + "! \n♥♥♥♥♥");
+
+DateTime time = DateTime.Now;
+Console.WriteLine("\nInicializated on " + time);
