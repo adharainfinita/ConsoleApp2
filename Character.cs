fa8aa21d21e4e1db944020f3c51d605ccde2cec6
Adhara <adharanosalevich@gmail.com>
2023-08-10 20:58:45 -0300

    Game first version complete

--- Character.cs
+++ Character.cs
@@ -8,9 +8,9 @@ public class Character
         Weapon = weapon;
         Rol = rol;
         Armor = armor;
-        Power = GetPower(weapon, rol);
-        Health = GetHealth(level, armor, rol); // Valor inicial de salud (por ejemplo)
-        Lifes = 3; // Cantidad inicial de vidas (por ejemplo)
+        Power = GetPower(weapon, rol); //Función para definir el poder del personaje
+        Health = GetHealth(level, armor, rol); // Valor inicial de salud
+        Lifes = 3; // Cantidad inicial de vidas 
         AreAlive = true; // Establecer inicialmente como vivo
     }
 
@@ -24,81 +24,127 @@ public class Character
     public int Lifes { get; set; }
     public bool AreAlive { get; set; }
 
-    public void TakeDamage(int damage) {
-
-        Health -= damage;
-        
-        if(Health <= 0 ){
-            Health = 0;
-            Lifes --;
-            if(Lifes <= 0){
-                AreAlive = false;
+    public void TakeDamage(int damage)
+    {
+        //Si la armadura es mayor al daño recibido, se le resta el daño a la armadura. Si la daña completamente
+        // Se establece en 0 para no tener números negativos
+        if (Armor > damage)
+        {
+            Armor -= damage;
+            if (Armor < 0)
+            {
+                Armor = 0;
             }
-            else {
-                Health = 100;
+        }
+        else
+        {
+            //Sino al valor final del daño recibido se le restará el valor de la armadura, y allí aplicará a la vida
+            damage -= Armor;
+            Health -= damage;
+
+            //Si la vida llega a cero, pierde una vida. 
+            if (Health <= 0)
+            {
+                Health = 0;
+                Lifes--;
+                if (Lifes <= 0)
+                {
+                    AreAlive = false;
+                }
+                else
+                {
+                    Health = 100;
+                }
             }
         }
     }
-    private int GetHealth(int health, int armor, string rol){
+    private int GetHealth(int level, int armor, string rol)
+    {
+        int health = 50;
 
-        switch(rol)
+        //Un rol tiene más vida que otro
+        switch (rol)
         {
             case "Warrior":
-            health =+10;
-            break;
+                health = +10;
+                break;
 
             case "Mage":
-            health =+ 5;
-            break;
+                health = +5;
+                break;
 
             case "Rogue":
-            health +=1;
-            break;
+                health += 1;
+                break;
         }
-    return health += armor;
+        //La armadura y el nivel aumentan la vida
+        return (health + armor) * level;
     }
-    
-    private int GetPower(string weapon, string rol){
+
+    private int GetPower(string weapon, string rol)
+    {
         int power = 10;
-        switch(weapon){
+
+        //Cada rol tiene un arma preferida, que aumentará su poder
+
+        switch (weapon)
+        {
             case "sword":
-            power+=10;
-            if(rol == "Warrior") power+=10;
-            break;
+                power += 10;
+                if (rol == "Warrior") power += 10;
+                break;
 
             case "magic wand":
-            power +=5;
-            if(rol == "Mage") power+=15;
-            break;
+                power += 5;
+                if (rol == "Mage") power += 15;
+                break;
 
             case "daggers":
-            power +=3;
-            if(rol =="Rogue") power +=17;
-            break;
+                power += 3;
+                if (rol == "Rogue") power += 17;
+                break;
 
             case "nothing":
-                power +=4;
+                power += 4;
                 break;
         }
         return power;
     }
 
-    public int Attack(Random accert, Character character)
+
+    public int Attack(Random accert, int power)
     {
         int successPercentage = accert.Next(0, 100);
+        
         Console.WriteLine("Atack succesful: %" + successPercentage);
-        if (successPercentage > 0)
+        
+        //Si el porcentaje de éxito es mayor a 95, se genera un golpe crítico. Mayor a 70, un buen golpe. 
+        if (successPercentage >= 95) power = (int)(power * (3 * successPercentage / 100.0));
+        if (successPercentage >= 70) power = (int)(power * (2 * successPercentage / 100.0));
+        if (successPercentage <70 &&  successPercentage > 0)
         {
-            character.Power = (int)(character.Power * (1 + successPercentage / 100.0));
+            power = (int)(power * (1 + successPercentage / 100.0));
         }
-        return character.Power;
+        return power;
     }
 
-    public int Deffend(Random accert, Character character)
+    public int Deffend(Random accert, int armor)
     {
-        int successPercentage = accert.Next(30, 100);
-        character.Armor = (int)(character.Armor * (1 + successPercentage / 100.0));
-        Console.WriteLine("Armor eleved: %" + successPercentage);
-        return character.Armor;
+
+        //Acción que aumenta la armadura con una probabilidad de éxito.
+        int successPercentage = accert.Next(10, 100);
+
+        if (armor == 0)
+        {
+            armor = 15;
+        }
+        else
+        {
+            armor = (int)(armor * (1 + successPercentage / 100.0));
+        }
+
+        Console.WriteLine("Armor elevated: %" + successPercentage);
+        return armor;
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -11,51 +11,56 @@ namespace ConsoleApp2
             string[] subjects = { "Orc", "Witch", "Wizzard", "Bear", "King" };
             string[] adjectives = { "Metal", "Fire", "Poison", "Water", "Darkness", "Veganism", "Death" };
             string[] roles = { "Mague", "Rogue", "Warrior" };
+            string[] weapons = { "sword", "magic wand", "daggers", "nothing" };
 
             Random number = new Random();
             string name = "The " + subjects[number.Next(0, subjects.Length)] + " " + adjectives[number.Next(0, adjectives.Length)];
             string rol = roles[number.Next(0, roles.Length)];
+            string weapon = weapons[number.Next(0, weapons.Length)];
 
-            return new Character(name, rol, "nothing", 3 * level, level);
+            int armor = 50 + level * 10;
+
+            return new Character(name, rol, weapon, armor, level);
 
         }
-        /**  private static bool SetTurn(int playerHealth, int enemyHealth)
-          {
-              if (playerHealth > enemyHealth) return true;
-              else return false;
-          }**/
+        public static Character GetCharacter(string name, int level)
+        {
+            string[] roles = { "Mague", "Rogue", "Warrior" };
+            string[] weapons = { "sword", "magic wand", "daggers", "nothing" };
+
+            Random number = new Random();
+            string rol = roles[number.Next(0, roles.Length)];
+            string weapon = weapons[number.Next(0, weapons.Length)];
+
+            int armor = 30 + level * 10;
+
+            return new Character(name, rol, weapon, armor, level);
+        }
         static void Main()
         {
-            Console.WriteLine("Write your first name: \t");
+            Console.WriteLine("Write your name: \t");
             string firstName = Console.ReadLine()!;
             Console.WriteLine("Hi, " + firstName + "! \n♥♥♥♥♥");
 
             DateTime time = DateTime.Now;
             Console.WriteLine("\nInitialized on " + time);
+          
 
-            Console.WriteLine("\nNow, What's your last Name?: \t");
-            string lastName = Console.ReadLine()!;
-            Console.WriteLine("How old are you?: \t");
-            string age = Console.ReadLine()!;
-            Console.Write("Do you know programming?");
-            Console.WriteLine(" Type Yes/No");
-            string develop = Console.ReadLine()!;
-
-            Console.WriteLine($"\nYour information:");
-            Console.WriteLine($"\tFirst Name: {firstName}\n\tLast Name: {lastName}\n\tAge: {age}\n\tDevelop: {(develop == "Yes" || develop == "yes")}");
-
-
+            //Selección de personaje
             Console.WriteLine("Please, select your character");
 
-            Character rabina = new Character("Rabina", "Mage", "magic wand", 25, 1);
-            Character dogi = new Character("Dogi", "Rogue", "daggers", 10, 1);
-            Character domini = new Character("Domini", "Warrior", "sword", 35, 1);
+
+            Character rabina = GetCharacter("Rabina", 1);
+            Character dogi = GetCharacter("Dogi", 1);
+            Character domini = GetCharacter("Domini", 1);
 
             Console.WriteLine("1." + domini.Name);
             Console.WriteLine("2." + dogi.Name);
             Console.WriteLine("3." + rabina.Name);
 
             int playerSelect = 0;
+
+            //Si no introducimos un caracter válido (1, 2 0 3) el sistema nos seleccionará automáticamente 1
             try
             {
                 playerSelect = int.Parse(Console.ReadLine());
@@ -65,21 +70,44 @@ namespace ConsoleApp2
                 Console.WriteLine(ex);
             }
 
+
             Character player;
             if (playerSelect == 1)
             {
                 Console.WriteLine("\nDomini has been selected\n");
                 player = domini;
+
+                //Vemos las estadísticas que tocó con ese pj.
+                //Es aleatorio, por lo que puede tocarnos un buen o mal set.
+
+                Console.WriteLine($@"...Stats...
+                          Health: {player.Health}
+                          Power: {player.Power}
+                          Rol: {player.Rol}
+                          Weapon: {player.Weapon}
+                          Armor: {player.Armor}");
             }
             else if (playerSelect == 2)
             {
                 Console.WriteLine("\nDogi has been selected\n");
                 player = dogi;
+                Console.WriteLine($@"...Stats...
+                          Health: {player.Health}
+                          Power: {player.Power}
+                          Rol: {player.Rol}
+                          Weapon: {player.Weapon}
+                          Armor: {player.Armor}");
             }
             else if (playerSelect == 3)
             {
                 Console.WriteLine("\nRabina has been selected\n");
                 player = rabina;
+                Console.WriteLine($@"...Stats...
+                          Health: {player.Health}
+                          Power: {player.Power}
+                          Rol: {player.Rol}
+                          Weapon: {player.Weapon}
+                          Armor: {player.Armor}");
             }
             else
             {
@@ -87,67 +115,134 @@ namespace ConsoleApp2
                 player = domini;
             }
 
-            Console.WriteLine("\nShort guide of the game:");
-            Console.WriteLine("\nFor each level, the enemies spawn randomly. \n The range of strength and lives increment. \n How many rounds will you survive?\n");
+            Console.WriteLine("\nSHORT GUIDE OF THE GAME:");
+            Console.WriteLine("\nFor each level, the enemies spawn randomly. " +
+                "\n The range of HEALTH and POWER increment.\n Your character not. But if you get +95% in attack" +
+                "the POWER incement." +
+                "\n\t\t\tHow many rounds will you survive?\n\n");
 
-            Console.WriteLine("\n--------------Level 1--------------------\n");
+            int level = 1;
+            int turns = 1;
 
-            Character enemyLvl1 = GetNewEnemy(1);
+            Console.WriteLine("+++++Your journey begins!+++++\n");
+
+            //Se genera un nuevo enemigo.
+            Character enemyLvl1 = GetNewEnemy(level);
+            Console.WriteLine($"\n--------------Level {level}--------------------\n");
             Console.WriteLine($"New enemy: {enemyLvl1.Name}, Role: {enemyLvl1.Rol}, Health: {enemyLvl1.Health}");
 
-            Console.WriteLine("Your journey begins!\n");
-
+            //Este será el flujo del juego. Mientras tu personaje viva, continuará.
             while (player.AreAlive)
             {
-   
-            
-                Console.WriteLine("Your turn\n");
+
+                Console.WriteLine("\n\t\t<<< Turn n°" + turns + " >>>>\n");
+
+                //Muestra las stats de ambos contrincantes
+                Console.WriteLine($"Your state... Health: {player.Health} | Armor: {player.Armor} | Power: {player.Power}\n");
+                Console.WriteLine($"Enemy state... Health: {enemyLvl1.Health} | Armor: {enemyLvl1.Armor} | Power: {enemyLvl1.Power}\n");
+
+                Console.WriteLine("\nYour turn.\n");
                 Console.WriteLine(@" 
                                1. Atack.
-                               2. Deffend.");
-                int action = int.Parse(Console.ReadLine());
-                Random accert = new Random();
+                               2. Deffend.
+                               3. Complete guide");
 
-                 if (action == 1)
+
+                try
                 {
-                    Console.WriteLine("--attack--\n");
-                   int damage = player.Attack(accert, player);
-                    enemyLvl1.TakeDamage(damage);
-                    Console.WriteLine("New live of enemy: " + enemyLvl1.Health + " | " + "Lifes: " + enemyLvl1.Lifes);
+                    int action = int.Parse(Console.ReadLine());
+
+                    //Se obtiene un número aleatorio para definir el acierto de la tirada del pj
+                    Random accert = new Random();
+
+
+                    if (action == 1)
+                    {
+                        Console.WriteLine("--attack--\n");
+
+                        
+                        int damage = player.Attack(accert, player.Power);
+                        enemyLvl1.TakeDamage(damage);
+                        Console.WriteLine($"Damage: {damage}");
+                        Console.WriteLine("New live of enemy: " + enemyLvl1.Health + " | " + "Lifes: " + enemyLvl1.Lifes);
+                    }
+                    else if (action == 2)
+                    {
+                        Console.WriteLine("--deffend--\n");
+                        int newArmor = player.Deffend(accert, player.Armor);
+                        player.Armor = newArmor;
+                        Console.WriteLine("New Armor: " + newArmor);
+
+                    }
+                    else if(action == 3) {
+                        Console.WriteLine("\tWelcome " + firstName);
+                        Console.WriteLine("-SPA-");
+                        Console.WriteLine("*El rol y el arma de tu personaje se generá de manera aleatoria. Esto brinda posibilidades\n buenas y malas, ya que cada rol tiene un arma preferida.");
+                        Console.WriteLine("*Tu personaje no sube de nivel");
+                        Console.WriteLine("*Si obtienes 70% de éxito 0 más tu ataque se duplica.");
+                        Console.WriteLine("*Si obtienes 95% de éxito o más tu ataque se triplica.");
+                        Console.WriteLine("*Al defender, también se genera un porcentaje de éxito. Este sirve para proteger tu vida");
+                        Console.WriteLine("Si la armadura es mayor al daño recibido, ésta absorbe el daño. Si se agota-inclusivo en la misma acción");
+                        Console.WriteLine("el resto lo recibirá la vida.");
+                        Console.WriteLine("Al derrotar a un enemigo, tu poder aumenta levemente.\n\n");
+                        Console.WriteLine("-ENG-");
+
+                        Console.WriteLine("*Your character's role and weapon will be generated randomly. This gives good and bad\n possibilities, as each role has a preferred weapon.");
+                        Console.WriteLine("*Your character doesn't level up");
+                        Console.WriteLine("*If you get 70% success or more your attack is doubled.");
+                        Console.WriteLine("*If you get 95% success or more your attack is tripled.");
+                        Console.WriteLine("*When defending, a success rate is also generated. This is used to protect your life");
+                        Console.WriteLine("If the armor is greater than the damage taken, it absorbs the damage. If it runs out-even in the same action");
+                        Console.WriteLine("life will receive the rest.");
+                        Console.WriteLine("When you defeat an enemy, your power is slightly increased.\n\n");
+                        Console.WriteLine("\nPress any key to continue...");
+                        Console.ReadKey();
+                    }
+
                 }
-                else if (action == 2)
+                catch (Exception e)
                 {
-                    int newArmor = player.Deffend(accert, player);
-                    Console.WriteLine("New Armor: " + newArmor);
-
+                    Console.WriteLine(e.ToString());
                 }
-            if(enemyLvl1.AreAlive)
-            {
-                Console.WriteLine("Turn of enemy\n");
-                Random number = new Random();
-               int autoAction = number.Next(1, 2);
-                //Random accert1 = new Random();//
 
-                if (autoAction == 1) 
+                //Acciones del enemigo
+                if (enemyLvl1.AreAlive)
                 {
-                    Console.WriteLine("--enemy attack--\n");
-                    int damage = enemyLvl1.Attack(accert, enemyLvl1);
-                    player.TakeDamage(damage);
-                    Console.WriteLine("New live of you: " + player.Health + " | " + "Lifes: " + player.Lifes);
+                    Console.WriteLine("\nTurn of enemy.\n");
 
-                }
-                else if (autoAction == 2) 
-                {
+                    Random accert = new Random();
+                    Random number = new Random();
+                    int autoAction = number.Next(1, 3);
+
+                    if (autoAction == 1)
+                    {
+                        Console.WriteLine("--enemy attack--\n");
+                        int damage = enemyLvl1.Attack(accert, enemyLvl1.Power);
+                        player.TakeDamage(damage);
+                        Console.WriteLine($"Damage: {damage}");
+                        Console.WriteLine("New live of you: " + player.Health + " | " + "Lifes: " + player.Lifes);
+
+                    }
+                    else if (autoAction == 2)
+                    {
                         Console.WriteLine("--enemy deffend--\n");
-                    int newArmor = enemyLvl1.Deffend(accert, enemyLvl1);
-                    Console.WriteLine("New Armor: " + newArmor);
+                        int newArmor = enemyLvl1.Deffend(accert, enemyLvl1.Armor);
+                        enemyLvl1.Armor = newArmor;
+                        Console.WriteLine("New Armor: " + newArmor);
+                    }
+
                 }
+                else
+                {
+                    Console.WriteLine($"You defeated {enemyLvl1.Name}! A new enemy has appeared.");
+                    player.Power += 5;
+                    level++;
+                    Console.WriteLine($"\n--------------Level {level}--------------------\n");
 
-            }
-               Character enemyLvl2 = GetNewEnemy(2);
-                Console.WriteLine($"You defeated {enemyLvl1.Name}! A new enemy has appeared.");
-                Console.WriteLine($"New enemy: {enemyLvl2.Name}, Role: {enemyLvl2.Rol}, Health: {enemyLvl2.Health}");
-
+                    enemyLvl1 = GetNewEnemy(level);
+                    Console.WriteLine($"New enemy: {enemyLvl1.Name}, Role: {enemyLvl1.Rol} , Health: {enemyLvl1.Health}");
+                }
+                turns++;
             }
             Console.WriteLine("Game over. You have been defeated.");
         }
